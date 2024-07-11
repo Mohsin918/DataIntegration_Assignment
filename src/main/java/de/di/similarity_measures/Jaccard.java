fec34f204f0f6e95b499258d8f9ca489231157a9
Mohsin/918 <mohsinquantum@gmail.com>
2024-07-11 20:48:21 +0200

    jaccard fix

--- src/main/java/de/di/similarity_measures/Jaccard.java
+++ src/main/java/de/di/similarity_measures/Jaccard.java
@@ -1,19 +1,15 @@
 package de.di.similarity_measures;
-
 import de.di.similarity_measures.helper.Tokenizer;
 import lombok.AllArgsConstructor;
-
 import java.util.*;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 @AllArgsConstructor
 public class Jaccard implements SimilarityMeasure {
-
     // The tokenizer that is used to transform string inputs into token lists.
     private final Tokenizer tokenizer;
-
     // A flag indicating whether the Jaccard algorithm should use set or bag semantics for the similarity calculation.
     private final boolean bagSemantics;
-
     /**
      * Calculates the Jaccard similarity of the two input strings. Note that the Jaccard similarity may use set or
      * multiset, i.e., bag semantics for the union and intersect operations. The maximum Jaccard similarity with
@@ -26,11 +22,11 @@ public class Jaccard implements SimilarityMeasure {
     public double calculate(String string1, String string2) {
         string1 = (string1 == null) ? "" : string1;
         string2 = (string2 == null) ? "" : string2;
-
         String[] strings1 = this.tokenizer.tokenize(string1);
         String[] strings2 = this.tokenizer.tokenize(string2);
         return this.calculate(strings1, strings2);
     }
+
 
     /**
      * Calculates the Jaccard similarity of the two string lists. Note that the Jaccard similarity may use set or
@@ -40,84 +36,71 @@ public class Jaccard implements SimilarityMeasure {
      * @param strings2 The second string list argument for the similarity calculation.
      * @return The multiset Jaccard similarity of the two arguments.
      */
+
+
     @Override
     public double calculate(String[] strings1, String[] strings2) {
         double jaccardSimilarity = 0;
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //                                      DATA INTEGRATION ASSIGNMENT                                           //
-        // Calculate the Jaccard similarity of the two String arrays. Note that the Jaccard similarity needs to be    //
-        // calculated differently depending on the token semantics: set semantics remove duplicates while bag         //
-        // semantics consider them during the calculation. The solution should be able to calculate the Jaccard       //
-        // similarity either of the two semantics by respecting the inner bagSemantics flag.                          //
-        //                                                                                                            //
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Calculate Jaccard similarity based on bag semantics
+        if (bagSemantics) {
+            Map<String, Integer> multisets1 = createMultiset(strings1);
+            Map<String, Integer> multisets2 = createMultiset(strings2);
 
-        if(!bagSemantics) {
-            //set semantic
-
-            Set<String> setStrings1 = new HashSet<>();
-            for(String str: strings1){
-                setStrings1.add(str);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //                                      DATA INTEGRATION ASSIGNMENT                                           //
+            // Calculate the Jaccard similarity of the two String arrays. Note that the Jaccard similarity needs to be    //
+            // calculated differently depending on the token semantics: set semantics remove duplicates while bag         //
+            // semantics consider them during the calculation. The solution should be able to calculate the Jaccard       //
+            // similarity either of the two semantics by respecting the inner bagSemantics flag.                          //
+            // Calculate intersection size
+            int intersectionSize = 0;
+            for (String key : multisets1.keySet()) {
+                if (multisets2.containsKey(key)) {
+                    intersectionSize += Math.min(multisets1.get(key), multisets2.get(key));
+                }
             }
 
-            //TODO:    compare performance of each method
-
-            Set<String> setStrings2 = new HashSet<>(Arrays.asList(strings2));
-
-            //intersection
-            Set<String> intersection = new HashSet<String>(setStrings1); // use the copy constructor
-            intersection.retainAll(setStrings2);
-            //TODO: was it shallow copy? did it change setStrings1?
-            //answer: shallow copy; didn't change setStrings1
-
-            jaccardSimilarity = (double)intersection.size()/(setStrings1.size()+setStrings2.size()-intersection.size());
-        } else {
-            //bag semantics or multi set semantics
-            Map<String, Integer> mapStrings1 = new HashMap<>();
-            Map<String, Integer> mapStrings2 = new HashMap<>();
-            //converting string array to dictionary
-            for(String str: strings1)
-                if(!mapStrings1.containsKey(str))
-                    mapStrings1.put(str, 1);
-                else
-                    mapStrings1.put(str, mapStrings1.get(str) + 1);
-
-            for(String str2: strings2)
-                if(!mapStrings2.containsKey(str2))
-                    mapStrings2.put(str2, 1);
-                else
-                    mapStrings2.put(str2, mapStrings2.get(str2) + 1);
-
-            //intersection
-            Map<String, Integer> mapIntersection = new HashMap<>(mapStrings1);
-            mapIntersection.keySet().retainAll(mapStrings2.keySet());
-            for (String str: mapIntersection.keySet()){
-                mapIntersection.put((str), Math.min(mapStrings1.get(str),mapStrings2.get(str)));
+            // Calculate union size
+            int unionSize = 0;
+            for (String key : multisets1.keySet()) {
+                unionSize += multisets1.get(key);
+            }
+            for (String key : multisets2.keySet()) {
+                unionSize += multisets2.get(key);
             }
 
-            int intersectionSum =0;
-            for(int i  :mapIntersection.values())
-                intersectionSum += i;
+            // Calculate Jaccard similarity
+            jaccardSimilarity = (double) intersectionSize / unionSize;
+        } else { // Calculate Jaccard similarity based on set semantics
+            Set<String> set1 = new HashSet<>(Arrays.asList(strings1));
+            Set<String> set2 = new HashSet<>(Arrays.asList(strings2));
 
+            //                                                                                                            //
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Calculate intersection size
+            int intersectionSize = 0;
+            for (String s : set1) {
+                if (set2.contains(s)) {
+                    intersectionSize++;
+                }
+            }
 
-            //sum of values in argument sets
-            int mapStrings1Size =0;
-            int mapStrings2Size =0;
+            // Calculate union size
+            int unionSize = set1.size() + set2.size() - intersectionSize;
 
-            for(int i  :mapStrings1.values())
-                mapStrings1Size += i;
-
-            for (int i  :mapStrings2.values())
-                mapStrings2Size += i;
-
-            //jaccard similarity
-            //bagging semantic union doesnt subtract intersection; sum of sets  //jaccardSimilarity = (double)intersectionSum/(mapStrings1Size+mapStrings2Size-intersectionSum);
-            jaccardSimilarity = (double)intersectionSum/(mapStrings1Size+mapStrings2Size);
-
+            // Calculate Jaccard similarity
+            jaccardSimilarity = (double) intersectionSize / unionSize;
         }
 
-
         return jaccardSimilarity;
+    }
+
+    private Map<String, Integer> createMultiset(String[] strings) {
+        Map<String, Integer> multisets = new HashMap<>();
+        for (String str : strings) {
+            multisets.put(str, multisets.getOrDefault(str, 0) + 1);
+        }
+        return multisets;
     }
 }
